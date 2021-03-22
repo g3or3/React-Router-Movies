65572a5bcaae0f89b71a8313622dc49ac03973a7
g3or3 <>
2021-03-22 16:57:53 -0400

    finished mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+import { Route, Switch } from "react-router";
 
-export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
-  const [movieList, setMovieList] = useState([]);
+export default function App() {
+	const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+	const [movieList, setMovieList] = useState([]);
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+	useEffect(() => {
+		const getMovies = () => {
+			axios
+				.get("http://localhost:5000/api/movies") // Study this endpoint with Postman
+				.then((response) => {
+					// Study this response with a breakpoint or log statements
+					// and set the response data as the 'movieList' slice of state
+					setMovieList(response.data);
+				})
+				.catch((error) => {
+					console.error("Server Error", error);
+				});
+		};
+		getMovies();
+	}, []);
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
+	const addToSavedList = (id) => {
+		// This is stretch. Prevent the same movie from being "saved" more than once
+	};
 
-  return (
-    <div>
-      <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList
+				list={
+					[
+						/* This is stretch */
+					]
+				}
+			/>
+			<Switch>
+				<Route path={"/movies/:id"}>
+					<Movie />
+				</Route>
+				<Route path={"/"}>
+					<MovieList movies={movieList} />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router";
 
 export default function SavedList(props) {
-  return (
-    <div className="saved-list">
-      <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
-      ))}
-      <div className="home-button">Home</div>
-    </div>
-  );
+	const history = useHistory();
+
+	return (
+		<div className="saved-list">
+			<h3>Saved Movies:</h3>
+			{props.list.map((movie) => (
+				<span className="saved-movie">{movie.title}</span>
+			))}
+			<div
+				className="home-button"
+				onClick={() => {
+					history.push("/");
+				}}
+			>
+				Home
+			</div>
+		</div>
+	);
 }
